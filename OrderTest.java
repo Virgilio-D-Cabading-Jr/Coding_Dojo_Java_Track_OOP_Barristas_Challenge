eda5ba03599a3e9c2644f2c1352cf354619b1944
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-11-29 18:10:32 -0800

    add 3 named orders

--- OrderTest.java
+++ OrderTest.java
@@ -18,12 +18,18 @@ public class OrderTest {
         orderList.add(new Order());
         printOrderList(orderList);
 
+        System.out.println("*****************************************************");
+        System.out.println("Create three orders of overloaded constructor with a name for each order\n");
+        orderList.add(new Order("Superman"));
+        orderList.add(new Order("Batman"));
+        orderList.add(new Order("Wonder Woman"));
+        printOrderList(orderList);
     }
 
     //  ---- Print Order List --------------------------------------
     public static void printOrderList (ArrayList<Order> orderList) {
         for (int i=0; i<orderList.size(); i++) {
-            System.out.printf("---- Order #%d ------------\n", i);
+            System.out.printf("---- Order #%d ------------\n", i+1);
             orderList.get(i).display();
         }
         System.out.println("");
